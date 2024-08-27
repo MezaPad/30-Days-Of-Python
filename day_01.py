b02c3f6c973897814908d94e071f401ecae7e1a1
IrmaPadilla <irmapaulinapadilla@gmail.com>
2024-08-27 15:31:04 -0600

    first commit

---
+++ day_01.py
@@ -0,0 +1,23 @@
+"""Provide some arithmetic functions"""
+
+A = 5
+B = 8
+
+print(A+B) # adition
+print(B-A) # substraction
+print(A%B) # modulus
+print(B/A) # division
+print(A**B) # exponential
+print(B//A) # floor division operator
+
+print('Astrid')
+print('Luxembourg')
+print('Belgien')
+print('I am enjoying 30 Days of python')
+
+print(type(10))
+print(type(9.8))
+print(type(3.4))
+print(type(4-4j))
+print(type(['Astrid', 'Python', 'Belgien']))
+print(type('Luxemburg'))
---
+++ day_02.py
@@ -0,0 +1,9 @@
+"""Lets see Built in functions and Variables"""
+#Built in functions
+#Data types
+#Checking data types and casting
+#Numbers
+
+# Print function will take multiple arguments
+# An argument is a value which we pass or put inside the function parenthesis
+FIRST_NAME = 'Astrid'
